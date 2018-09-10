f69988933296d42205f9c1181f4c78588a2cedda
RMSanchez <raulsg93@gmail.com>
2018-09-09 23:24:14 -0500

    ports for Heroku deployment

--- server.js
+++ server.js
@@ -27,7 +27,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHomework");
+// mongoose.connect("mongodb://localhost/mongoHomework");
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHomework";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
